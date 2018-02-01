014234a0e4c9e71ef3d2c4059ba0e517fa66cbd3
Justin Neil <justinmneil@gmail.com>
2018-02-01 11:33:11 -0500

    finished secret handshake

--- secret-handshake/secret_handshake.exs
+++ secret-handshake/secret_handshake.exs
@@ -15,6 +15,16 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    rdigits = Enum.reverse(Integer.digits(code,2))
+    list = []
+    if Enum.at(rdigits, 0) == 1, do: list = list ++ ["wink"]
+    if Enum.at(rdigits, 1) == 1, do: list = list ++ ["double blink"]
+    if Enum.at(rdigits, 2) == 1, do: list = list ++ ["close your eyes"]
+    if Enum.at(rdigits, 3) == 1, do: list = list ++ ["jump"]
+    if Enum.at(rdigits, 4) == 1, do: list = Enum.reverse(list)
+    list
   end
+
 end
 
+# SecretHandshake.commands(12) |> IO.inspect
