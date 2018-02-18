24f4602d108d7b42ce3187aa762cda5486d0e604
Justin Neil <justinmneil@gmail.com>
2018-02-17 20:33:34 -0500

    Completed Rotational Cipher

--- rotational-cipher/rotational_cipher.exs
+++ rotational-cipher/rotational_cipher.exs
@@ -8,6 +8,22 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    Enum.map(to_charlist(text), fn (char) -> rot(char, shift) end)
+    |> to_string
+  end
+
+  def rot(char, shift) when (char>=65 and char<=90) or (char>=97 and char<=122) do
+    rotated = char + shift
+    
+    cond do
+      (char>=65 and char<=90) and rotated > 90 -> <<rotated-26>>
+      (char>=97 and char<=122) and rotated > 122 -> <<rotated-26>>
+      true -> <<rotated>>
+    end
+  end
+
+  def rot(char, _shift) do
+    char
   end
 end
 
